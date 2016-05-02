02a51a13c4a0eb7ce16dcfa7a46a6498fae095c5
Erlend ter Maat <erwitema@gmail.com>
2016-05-02 21:33:48 +0200

    Jay! This flies

--- src/FlyingDutchman/Log.php
+++ src/FlyingDutchman/Log.php
@@ -4,16 +4,63 @@ namespace FlyingDutchman;
 
 class Log {
   protected $distance;
+  protected $landDistance;
+  protected $translationX;
+  protected $translationY;
+  protected $translationZ;
+  protected $direction;
 
-  public function go($time, $speed=1) {
+  public function __construct() {
+    $this->distance = 0;
+    $this->landDistance = 0;
+    $this->translationX = 0;
+    $this->translationY = 0;
+    $this->translationZ = 0;
+    $this->course = 0;
+    $this->direction = 0;
+  }
+
+  public function go($time, $speed=1, $angle=0) {
     $distance = $time * $speed;
 
     $this->distance = $distance + $this->distance;
+    $landDistance = ($distance * cos(deg2rad($angle)));
+    $this->landDistance += $landDistance;
+    $this->translationX += $landDistance * cos(deg2rad($this->direction));
+    $this->translationY += $landDistance * sin(deg2rad($this->direction));
+    $this->translationZ += ($distance * sin(deg2rad($angle)));
 
-    return $distance;
+    return $this;
   }
 
   public function getOdo() {
     return $this->distance;
+  }
+
+  public function getLandDistance() {
+    return $this->landDistance;
+  }
+
+  public function getAltitude() {
+    return $this->translationZ;
+  }
+
+  public function changeCourse($deg) {
+    $this->direction += $deg;
+    while ($this->direction >= 360) {
+      $this->direction -= 360;
+    }
+    while ($this->direction < 0) {
+      $this->direction += 360;
+    }
+    return $this;
+  }
+
+  public function getDirection() {
+    return $this->direction;
+  }
+
+  public function getLocation() {
+    return [$this->translationX,$this->translationY];
   }
 }
--- test/FlyingDutchman/LogTest.php
+++ test/FlyingDutchman/LogTest.php
@@ -5,40 +5,67 @@ use FlyingDutchman\Log;
 
 class LogTest extends \PHPUnit_Framework_TestCase {
 
-  public function testShouldFly() {
-    $log = new Log();
-
-    // will it fly?
-    $distance = $log->go(0);
-    $this->assertEquals(0, $distance);
-  }
-
   public function testShouldFlyFar() {
     $log = new Log();
 
     // will it fly?
-    $distance = $log->go(1);
-    $this->assertEquals(1, $distance);
+    $log->go(1);
+    $this->assertEquals(1, $log->getOdo());
 
     $log->go(1);
-    $odo = $log->getOdo();
-    $this->assertEquals(2, $odo);
+    $this->assertEquals(2, $log->getOdo());
 
     $log->go(1);
-    $odo = $log->getOdo();
-    $this->assertEquals(3, $odo);
+    $this->assertEquals(3, $log->getOdo());
   }
 
-  public function testShouldFlyFast() {
+  public function testShouldFlyForward() {
     $log = new Log();
 
-    // will it fly?
-    $distance = $log->go(1, 2);
-    $this->assertEquals(2, $distance);
-    
-    // continue to fly
-    $distance = $log->go(2, 3);
-    $this->assertEquals(6, $distance);
+    $log->go(0);
+    $this->assertEquals(0, $log->getLandDistance());
+
+    $log->go(1);
+    $this->assertEquals(1, $log->getLandDistance());
+
+    $log->go(1, sqrt(2), 45);
+    $this->assertEquals(2, $log->getLandDistance());
+
   }
-  
+
+  public function testShouldFlyUp() {
+    $log = new Log();
+
+    $log->go(1, 1, 0);
+    $this->assertEquals(0, $log->getAltitude());
+    
+    $distance = $log->go(1, sqrt(1.25), rad2deg(atan(.5)));
+    $this->assertEquals(.5, round($log->getAltitude(), 3));
+  }
+
+  public function testShouldFlyInACertainDirection() {
+    $log = new Log();
+
+    $this->assertEquals(0, $log->changeCourse(0)->getDirection());
+    $this->assertEquals(10, $log->changeCourse(10)->getDirection());
+    $this->assertEquals(20, $log->changeCourse(10)->getDirection());
+    $this->assertEquals(20, $log->changeCourse(360)->getDirection());
+    $this->assertEquals(0, $log->changeCourse(700)->getDirection());
+    $this->assertEquals(0, $log->changeCourse(-360)->getDirection());
+    $this->assertEquals(0, $log->changeCourse(-720)->getDirection());
+    $this->assertEquals(359, $log->changeCourse(-1)->getDirection());
+  }
+
+  public function testShouldBeSomewhere() {
+    $log = new Log();
+
+    $this->assertEquals([1,0], $log->go(1)->getLocation());
+    $this->assertEquals([2,0], $log->go(1)->getLocation());
+    $log->changeCourse(90);
+
+    $this->assertEquals([2,1], $log->go(1)->getLocation());
+
+    $log->changeCourse(90)->go(2)->changeCourse(90)->go(1);
+    $this->assertEquals([0,0], $log->getLocation());
+  }
 }
